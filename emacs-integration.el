49a92886f106ce61555718e6ce609d8f9b500814
Frank Terbeck <ft@bewatermyfriend.org>
2012-08-16 16:23:35 +0200

    Add emacs integration script
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ emacs-integration.el
@@ -0,0 +1,11 @@
+;; Load this file into emacs for proper indentation of the project's macros
+;; within emacs' `scheme-mode'.
+;;
+;; This file is part of `scmtap'.
+
+(mapc (lambda (thing)
+        (put (car thing)
+             'scheme-indent-function
+             (cdr thing)))
+      '((with-test-bundle . 1)
+        (define-test . 1)))
