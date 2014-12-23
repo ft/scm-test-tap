9739769a6c0f7d86b855887580916caacb274d0d
Frank Terbeck <ft@bewatermyfriend.org>
2014-12-23 11:37:40 +0100

    Add ‘skip’ test cases for ‘define-test’
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ test/skip.scm
@@ -0,0 +1,14 @@
+;; Copyright (c) 2014 Frank Terbeck <ft@bewatermyfriend.org>
+;; All rights reserved.
+;; Terms for redistribution and use can be found in LICENCE.
+
+(use-modules (test tap))
+
+(with-test-bundle (test tap skip)
+  (plan 3)
+  (define-test skip "This test will be skipped"
+    (pass-if-true (format #t "not ok 0 - This should not be printed.~%")))
+  (define-test skip "This test will be skipped as well"
+    (pass-if-true (format #t "not ok 0 - Neither should this...~%")))
+  (define-test "This one will NOT be skipped"
+    (pass-if-= 1 1)))
