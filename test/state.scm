1d6ac5142fc3722bfdc98be16c797c2b6dcce026
Frank Terbeck <ft@bewatermyfriend.org>
2016-10-20 22:54:57 +0200

    Add test to checks stateful expressions
    If an expression changs the internal state of a machine, the system will
    fail if a test evaluates an expression more than once. In this test, a
    simple counter procedure is used to make sure that doesn't happen.
    
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

---
+++ test/state.scm
@@ -0,0 +1,27 @@
+;; Copyright (c) 2016 Frank Terbeck <ft@bewatermyfriend.org>
+;; All rights reserved.
+;; Terms for redistribution and use can be found in LICENCE.
+
+(use-modules (test tap))
+
+(define counter #f)
+
+(let ((state 0))
+  (set! counter
+    (lambda ()
+      (let ((old state)
+            (new (+ state 1)))
+        (set! state new)
+        old))))
+
+(with-test-bundle (test tap state)
+  (plan 3)
+
+  (define-test "Use a counter with internal state works"
+    (pass-if-= (counter) 0))
+
+  (define-test "Use the next value from said counter is correct"
+    (pass-if-= (counter) 1))
+
+  (define-test "...and a third time for good measure"
+    (pass-if-= (counter) 2)))
